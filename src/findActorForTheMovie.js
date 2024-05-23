65b0876a4511a2a46ed16870513667a785107121
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 13:20:34 -0300

    findActorForTheMovie done

--- src/findActorForTheMovie.js
+++ src/findActorForTheMovie.js
@@ -2,6 +2,17 @@ const movies = require("../data/movies");
 
 const findActorForTheMovie = (titulo) => {
   
+  if(!titulo){
+    return "Filme não encontrado.";
+  }
+
+const titulos = movies.find(item=> item.titulo.toUpperCase() === titulo.toUpperCase());
+
+if (!titulos) {
+  return "Filme não encontrado.";
+}
+
+return titulos.elenco.slice(0,2)
 }
 
 module.exports = {
