2407149b0fb0f231f907948a11587eb86f5a622c
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 12:39:06 -0300

    titlesAndDirectors

--- src/titlesAndDirectors.js
+++ src/titlesAndDirectors.js
@@ -1,9 +1,10 @@
 const { movies } = require("../data/list1");
 
 const titlesAndDirectors = (movies) => {
- 
+return movies.forEach(({titulo , diretor}) => {
+    console.log(`Título: ${titulo} - Diretor: ${diretor}`);
+});
 }
-
 module.exports = {
   titlesAndDirectors
 }
