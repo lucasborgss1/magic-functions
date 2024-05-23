e67b63c9332f08dab53670d5ffcbab74486575ee
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 13:38:33 -0300

    countGenres done

--- src/countGenres.js
+++ src/countGenres.js
@@ -1,7 +1,20 @@
 const movies = require("../data/movies");
 
 function countGenres(movies) {
- 
+
+  if (movies.length === 0) {
+    return {};
+  }
+
+  return movies.reduce((accumulator, movies)=>{
+if (movies.genero && movies.genero.length > 0) {
+  movies.genero.forEach(genero => {
+    accumulator[genero] = (accumulator[genero] || 0) +1
+  });
+}
+return accumulator;
+}, {});
+
 }
 
 module.exports = {
