8d7f7631195200b1b4fb274eb2d7026f80fca76a
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 13:00:18 -0300

    nolanFilms done

--- src/nolanFilms.js
+++ src/nolanFilms.js
@@ -1,7 +1,7 @@
 const movies = require("../data/movies");
 
 const nolanFilms = (movies) => {
- 
+ return movies.filter(({diretor}) => diretor == 'Christopher Nolan' ).sort((a, b) => b.avaliacao - a.avaliacao)
 }
 
 module.exports = {
