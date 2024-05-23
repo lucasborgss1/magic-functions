a55de6a420f6bd677b6a78f6254c297974dd8633
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 14:12:13 -0300

    averageRatingByGenre done

--- src/averageRatingByGenre.js
+++ src/averageRatingByGenre.js
@@ -1,6 +1,26 @@
 const movies = require("../data/movies");
 function averageRatingByGenre(genero) {
+if(!genero){
+  return "O parâmetro é obrigatório.";
+}
+if(typeof genero !== 'string'){
+return "O parâmetro tem que ser uma string.";
+}
+if (genero.trim() === '') {
+  return "O parâmetro é obrigatório.";
+}
 
+const generos = movies.filter(movies => movies.genero.includes(genero));
+
+if (generos.length === 0) {
+  return `Não há filmes do gênero ${genero}.`
+}
+
+const totalAvaliacao = generos.reduce((accumulator, filme) => accumulator + filme.avaliacao, 0)
+mediaAvaliacao = totalAvaliacao / generos.length;
+numeroFormatado = mediaAvaliacao.toFixed(2);
+
+return `A média de avaliação dos filmes do gênero ${genero} é ${numeroFormatado}.`
 }
 
 module.exports = { 
