ae33bd6ecf96042f50b877b7eab3e2fed81b784e
Lucas de Almeida Borges <lucasdealmeidaborges123@gmail.com>
2024-05-23 12:56:25 -0300

    filmsAvailable done

--- src/filmsAvailable.js
+++ src/filmsAvailable.js
@@ -1,7 +1,7 @@
 const movies = require("../data/movies");
 
 const filmsAvailable = (movies) => {
- 
+return movies.filter(({disponivel})=> disponivel == true).map(({titulo})=> titulo)
 }
 
 module.exports = {
